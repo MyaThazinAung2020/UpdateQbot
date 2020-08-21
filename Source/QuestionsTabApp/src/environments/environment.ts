6b0a01e787dd76dca87df788d1e122db4e1e3ccd
Mya Thazin Aung <myathazin.aung@alfaconnections.com>
2020-08-21 13:03:34 +0630

    f com

---
+++ Source/QuestionsTabApp/src/environments/environment.ts
@@ -0,0 +1,25 @@
+export const environment = {
+    production: false,
+    apiBaseUrl: "https://mthazinaqna.azurewebsites.net/api/Request/",
+    selfUrl: "https://mthazinaqna-questions.azurewebsites.net",
+
+    authConfig: {
+        instance: "https://login.microsoftonline.com/",
+        tenantId: "aa81ea33-cf73-444b-b305-006b188a8fb4",
+        clientId: "383fb702-c98c-482b-bb9c-4ee1270b6379",
+        redirectUri: "/app-silent-end",
+        cacheLocation: "localStorage",
+        navigateToLoginRequestUrl: false,
+        extraQueryParameters: "",
+        popUp: true,
+        popUpUri: "/app-silent-start",
+        popUpWidth: 600,
+        popUpHeight: 535
+    },
+
+    // do not populate the following:
+    upn: "",
+    tid: "",
+    gid: "",
+    cname: ""
+};
